8b975bd2fde11e147d361bd929fd884cb1812c9b
rayhan7786 <mdrayhanparvezrokey@gmail.com>
2026-03-28 19:50:39 +0600

    first commit

---
+++ Fast code.py
@@ -0,0 +1,23 @@
+print("Hello World") #This is a comment
+x=5
+y="Joy"
+print(x)
+print(y)
+
+
+x=str(4)
+y=int(50)
+z=(" The art is code.")
+print(type(x)) #This is a string.
+
+print(type(y)) #This is a int.  
+
+print(type(z)) #this is a Float.
+
+#Name ki word print.
+myvar='joy'
+my_var="Joy"
+print(myvar)
+print(my_var)
+name="rayhan parvez rokey"
+print(name)
---
+++ data_type.py
@@ -0,0 +1,23 @@
+# data type 
+Fast_Name = "Rayhan parvez Rokey"
+address = 'Chattogram'
+email = 'mohamedrayhanrokey102@gmail.com'
+print(Fast_Name,address,email)
+
+# int type data
+
+Hello = 420
+print(Hello)
+
+#Float data type
+hello = 4420.2
+print(hello)
+
+# complex type data type
+hello = 2j
+print(hello)
+
+# String type data
+a = 'Hello'
+b = 'World'
+print("This is arth" + ' ' + a)
