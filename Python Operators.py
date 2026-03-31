2553f4e761c0e5e6d186adc735935c7e51a6393c
rayhan7786 <mdrayhanparvezrokey@gmail.com>
2026-03-31 19:47:32 +0600

    new code add

---
+++ Python Operators.py
@@ -0,0 +1,36 @@
+#Arithmetic Operators........
+#addition operators
+a = 50
+b = 30
+print(a+b)
+
+#subtraction operators
+print(a-b)
+
+#multiplication operators
+print(a*b)
+
+#division operators
+print(a/b)
+
+#modulus operators
+print(a%b)
+
+#exponentiation operators
+print(a**b)
+
+#Floor division operators
+print(a//b)
+
+#Assignment Operators.........
+x1 = 7
+y1 = 3
+print(x1>y1)
+
+
+#sowaping
+c = 500
+d = 600
+c,d = d,c
+print("the number is c", c)
+print("the number is d", d) #this is swapint .......
--- data_type.py
+++ data_type.py
@@ -1,4 +1,4 @@
-# data type 
+"""# data type 
 Fast_Name = "Rayhan parvez Rokey"
 address = 'Chattogram'
 email = 'mohamedrayhanrokey102@gmail.com'
@@ -21,3 +21,37 @@ print(hello)
 a = 'Hello'
 b = 'World'
 print("This is arth" + ' ' + a)
+"""
+
+# Bool data type 
+hello = True
+print(type(hello))
+
+hello1 = False
+print(type(hello1))
+
+x=10
+y=20
+print(x<y)
+
+# string Formatting
+num1 = 200
+num2 = 3000
+print(f"sum {num1+num2}")
+
+# bytes data type
+hello4 = [1,3,3,4,5,5,100]
+n = bytes(hello4)
+print(n)
+
+#bytearray
+harun = [1,3,22,22,33,]
+harun1 = bytearray(harun)
+harun1 [2] = 10
+print(harun1[2])
+
+#range data type
+
+run = range(100)
+for i in run:
+    print(i)
