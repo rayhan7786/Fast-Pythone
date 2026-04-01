5bf69af62556d4fdc32e783fc4c163bcc50e552e
rayhan7786 <mdrayhanparvezrokey@gmail.com>
2026-04-01 20:07:14 +0600

    New code Update

---
+++ User Input.py
@@ -0,0 +1,19 @@
+#user input
+Name = input("Enter your name:")
+
+Email = input("Enter your email:")
+
+Address = input("Enter your address:")
+
+Phone_number = input("Enter your Phone_number")
+
+password = input  ("Enter your password")
+print(Name)
+print(Email)
+print(Address)
+print(Phone_number)
+print(password)
+
+# Input number in user......
+
+
